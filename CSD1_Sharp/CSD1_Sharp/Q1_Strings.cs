6540186a4463b298ee5da3d716059601a3e06ee8
shamanhex <shaman.hex@gmail.com>
2019-01-23 00:12:52 +0600

    + Добавлен вопрос №1 про строки. + Добавлен черновик вопроса №2

---
+++ CSD1_Sharp/CSD1_Sharp/Q1_Strings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CSD1_Sharp
+{
+    [TestFixture]
+    public class Q1_Strings
+    {
+        string s1, s2, s3;
+
+        [SetUp]
+        public void Init()
+        {
+            s1 = string.Format("{0}{1}", "abc", "cba");
+            s2 = "abc" + "cba";
+            s3 = "abccba";
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(s1 == s2);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.IsTrue((object)s1 == (object)s2);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.IsTrue(s2 == s3);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Assert.IsTrue((object)s2 == (object)s3);
+        }
+    }
+}
---
+++ CSD1_Sharp/CSD1_Sharp/Q2_Patterns.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CSD1_Sharp
+{
+    [TestFixture]
+    public class Q2_Patterns
+    {                       
+        int Square(int x = 10)
+        {
+            return x * x;
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(100, this.Square());
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(0, this.Square(0));
+        }
+        
+
+    }
+}
