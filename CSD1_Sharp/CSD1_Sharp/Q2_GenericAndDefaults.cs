b354925b8f90b753a55907090d9c65a0fd3c776b
shamanhex <shaman.hex@gmail.com>
2019-01-24 00:09:39 +0600

    Add second question Q2_GenericAndDefaults.cs

---
+++ CSD1_Sharp/CSD1_Sharp/Q2_GenericAndDefaults.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CSD1_Sharp
+{
+    [TestFixture]
+    public class Q2_GenericAndDefaults
+    {
+        bool Eq<T>(object arg1 = null, T arg2 = default(T))   
+        {                            
+            return Object.Equals(arg1, arg2);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(Eq<int>(1));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.IsTrue(Eq<object>());
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.IsTrue(Eq<String>(""));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Assert.IsTrue(Eq<String>());
+        }  
+    }
+}
