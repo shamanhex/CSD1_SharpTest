f12b9947110dc0d3d508c5802c65f5202dd53396
shamanhex <shaman.hex@gmail.com>
2019-01-29 23:56:06 +0600

    + Добавлен вопрос #3 про передачу параметров функцию. + Добавлен вопрос #4 про исключения (только файл, без реализации)

---
+++ CSD1_Sharp/CSD1_Sharp/Q4_Exception.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CSD1_Sharp
+{
+    [TestFixture]
+    public class Q4_Exception
+    {
+        void Devide(int x)
+        {
+            x += 10;
+        }
+
+        [Test]
+        public void Test1()
+        {
+            int x = 20;
+            F1(x);
+            Assert.AreEqual(20, x);
+        }
+
+        void F2(ref int x)
+        {
+            x += 10;
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int x = 20;
+            F2(ref x);
+            Assert.AreEqual(20, x);
+        }       
+        
+        void F3(out int x)
+        {
+            x = 10;
+        }
+
+        [Test]
+        public void Test3()
+        {             
+            int x = 20;
+            F3(out x);
+            Assert.AreEqual(20, x);
+        }
+
+        int F4(params int[] x)
+        {
+            return x.Sum();
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Assert.AreEqual(10, F4(10, 20, 30));
+        }  
+    }
+}
