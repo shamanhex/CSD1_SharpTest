7dd45123a47bf8a8f698e6600b55f6b86fbcfd26
danilov-sv <danilovsv@mail.ru>
2019-02-01 06:21:18 +0600

    + Linq + yield

---
+++ CSD1_Sharp/CSD1_Sharp/Question_LINQ.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpExam
+{
+    [TestClass]
+    public class Question_LINQ
+    {
+        IEnumerable<int> odd_numbers1, odd_numbers2;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            List<int> numbers = new List<int>() { 0, 1, 2, 3, 4 };
+
+            odd_numbers1 = from n in numbers
+                           where n % 2 == 0
+                           select n;
+            odd_numbers2 = odd_numbers1.ToList();
+            numbers.Remove(2);
+        }
+
+        [TestMethod]
+        public void Answer1()
+        {
+            CollectionAssert.AreEqual(new []{ 0, 2, 4 }, odd_numbers1.ToList());
+            CollectionAssert.AreEqual(new []{ 0, 2, 4 }, odd_numbers2.ToList());
+        }
+
+        [TestMethod]
+        public void Answer2()
+        {
+            CollectionAssert.AreEqual(new []{ 0, 4 }, odd_numbers1.ToList());
+            CollectionAssert.AreEqual(new []{ 0, 2, 4 }, odd_numbers2.ToList());
+        }
+
+        [TestMethod]
+        public void Answer3()
+        {
+            CollectionAssert.AreEqual(new []{ 0, 2, 4 }, odd_numbers1.ToList());
+            CollectionAssert.AreEqual(new []{ 0, 4 }, odd_numbers2.ToList());
+        }
+
+        [TestMethod]
+        public void Answer4()
+        {
+            CollectionAssert.AreEqual(new []{ 0, 4 }, odd_numbers1.ToList());
+            CollectionAssert.AreEqual(new []{ 0, 4 }, odd_numbers2.ToList());
+        }
+
+    }
+}
---
+++ CSD1_Sharp/CSD1_Sharp/Question_yield.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpExam
+{
+    [TestClass]
+    public class Question_yield
+    {
+        List<string> checkpoints1, checkpoints2;
+
+        IEnumerable<string> _fizzbuzz(List<string> checkpoints)
+        {
+            checkpoints.Add("A");
+            yield return "fizz";
+            checkpoints.Add("B");
+            yield return "buzz";
+            checkpoints.Add("C");
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            checkpoints1 = new List<string>();
+            foreach (string value in _fizzbuzz(checkpoints1))
+                checkpoints1.Add(value);
+
+            checkpoints2 = new List<string>();
+            checkpoints2.AddRange(_fizzbuzz(checkpoints2));
+        }
+
+
+        [TestMethod]
+        public void Answer1()
+        {
+            CollectionAssert.AreEqual(new[] { "A", "fizz", "B", "buzz", "C" }, checkpoints1);
+            CollectionAssert.AreEqual(checkpoints1, checkpoints2);
+        }
+
+        [TestMethod]
+        public void Answer2()
+        {
+            CollectionAssert.AreEqual(new[] { "A", "B", "C", "fizz", "buzz" }, checkpoints1);
+            CollectionAssert.AreEqual(checkpoints1, checkpoints2);
+        }
+
+        [TestMethod]
+        public void Answer3()
+        {
+            CollectionAssert.AreEqual(new[] { "A", "fizz", "B", "buzz", "C" }, checkpoints1);
+            CollectionAssert.AreNotEqual(checkpoints1, checkpoints2);
+        }
+
+        [TestMethod]
+        public void Answer4()
+        {
+            CollectionAssert.AreEqual(new[] { "A", "B", "C", "fizz", "buzz" }, checkpoints1);
+            CollectionAssert.AreNotEqual(checkpoints1, checkpoints2);
+        }
+    }
+}
