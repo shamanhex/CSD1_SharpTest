b91edd18f873e18f664769cc7d7e84689b36fcef
danilov-sv <danilovsv@mail.ru>
2019-02-03 08:29:17 +0600

    ~ yield (изменен подход к тесту)

--- CSD1_Sharp/CSD1_Sharp/Question_yield.cs
+++ CSD1_Sharp/CSD1_Sharp/Question_yield.cs
@@ -8,8 +8,6 @@ namespace CSharpExam
     [TestFixture]
     public class Question_yield
     {
-        List<string> checkpoints1, checkpoints2;
-
         IEnumerable<string> _fizzbuzz(List<string> checkpoints)
         {
             checkpoints.Add("A");
@@ -19,44 +17,26 @@ namespace CSharpExam
             checkpoints.Add("C");
         }
 
-        [SetUp]
-        public void TestInit()
-        {
-            checkpoints1 = new List<string>();
-            foreach (string value in _fizzbuzz(checkpoints1))
-                checkpoints1.Add(value);
-
-            checkpoints2 = new List<string>();
-            checkpoints2.AddRange(_fizzbuzz(checkpoints2));
-        }
-
 
         [Test]
         public void Answer1()
         {
+            var checkpoints1 = new List<string>();
+            foreach (string value in _fizzbuzz(checkpoints1))
+                checkpoints1.Add(value);
+
+
             CollectionAssert.AreEqual(new[] { "A", "fizz", "B", "buzz", "C" }, checkpoints1);
-            CollectionAssert.AreEqual(checkpoints1, checkpoints2);
         }
 
         [Test]
         public void Answer2()
         {
-            CollectionAssert.AreEqual(new[] { "A", "B", "C", "fizz", "buzz" }, checkpoints1);
-            CollectionAssert.AreEqual(checkpoints1, checkpoints2);
-        }
+            var checkpoints1 = new List<string>();
+            checkpoints1.AddRange(_fizzbuzz(checkpoints1));
 
-        [Test]
-        public void Answer3()
-        {
             CollectionAssert.AreEqual(new[] { "A", "fizz", "B", "buzz", "C" }, checkpoints1);
-            CollectionAssert.AreNotEqual(checkpoints1, checkpoints2);
         }
 
-        [Test]
-        public void Answer4()
-        {
-            CollectionAssert.AreEqual(new[] { "A", "B", "C", "fizz", "buzz" }, checkpoints1);
-            CollectionAssert.AreNotEqual(checkpoints1, checkpoints2);
-        }
     }
 }
