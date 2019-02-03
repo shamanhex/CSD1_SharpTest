be11ce2596c0879d5fcd76a6570c01625dbac495
danilov-sv <danilovsv@mail.ru>
2019-02-03 09:41:12 +0600

    + Struct types versus Reference types

---
+++ CSD1_Sharp/CSD1_Sharp/Q5_StructVsRefs.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSD1_Sharp
+{
+    class C : IDisposable
+    {
+        public bool Disposed { get; set; }
+        public void Dispose() { Disposed = true; }
+    }
+
+    struct S : IDisposable
+    {
+        public bool Disposed { get; set; }
+        public void Dispose() { Disposed = true; }
+    }
+
+
+    [TestFixture]
+    public class Q5_StructVsRefs
+    {
+        void OuterDispose(C c)
+        {
+            c.Disposed = true;
+        }
+
+        void OuterDispose(S s)
+        {
+            s.Disposed = true;
+        }
+
+
+        [Test]
+        public void Test1()
+        {
+            var c = new C();
+            OuterDispose(c);
+            var s = new S();
+            OuterDispose(s);
+            Assert.IsTrue(c.Disposed);
+            Assert.IsTrue(s.Disposed);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var c = new C();
+            var other_c = c;
+            c.Dispose();
+            Assert.IsTrue(other_c.Disposed);
+
+            var s = new S();
+            var other_s = s;
+            s.Dispose();
+            Assert.IsTrue(other_s.Disposed);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var c = new C();
+            using (c) { /*nothing to do*/ }
+            Assert.IsTrue(c.Disposed);
+
+            var s = new S();
+            using (s) { /* nothing to do */ }
+            Assert.IsTrue(s.Disposed);
+        }
+
+        [Test]
+        public void Test4()
+        {
+        }
+
+    }
+}
